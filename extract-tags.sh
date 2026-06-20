ef5e30fe69b35776feb67bba27c6dfade539e712
Roger Keller <rokeller@users.noreply.github.com>
2026-06-20 14:18:42 +0200

    (fix) Add more details in release workflow (#31)

--- extract-tags.sh
+++ extract-tags.sh
@@ -2,16 +2,15 @@
 
 REF_NAME="$1"
 if [ -z "$REF_NAME" ]; then
-    echo 'The <ref_name> paramter is required.'
+    echo "The <ref_name> paramter is required. ($REF_NAME)"
     exit 1
 fi
 
 SEMVER_REGEX='^v?([0-9]+)\.([0-9]+)\.([0-9]+)$'
 if ! [[ "$REF_NAME" =~ $SEMVER_REGEX ]]; then
-    echo 'Only <major>.<minor>.<patch> semver release versions are supported.'
+    echo "Only <major>.<minor>.<patch> semver release versions are supported. ($REF_NAME)"
   exit 1
 fi
-
 
 SEMVER_COMPONENTS_STR=$(echo "$REF_NAME" | sed -E "s/$SEMVER_REGEX/\1 \2 \3/")
 read -a SEMVER_COMPONENTS <<< "$SEMVER_COMPONENTS_STR"
